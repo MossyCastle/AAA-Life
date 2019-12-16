a1521a070cce4919688075bb9ebae231b8401bad
MossyCastle <46568873+MossyCastle@users.noreply.github.com>
2019-12-16 15:59:25 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -29,9 +29,10 @@ resource "aws_instance" "web" {
     ami = data.aws_ami.amazon_linux_2.id
     instance_type = "t2.micro"
     security_groups = [aws_security_group.web_sg.id]
-    user_data = file("bootstrap.sh")
+    **'user_data = file("bootstrap.sh")
     tags = {
-        Name = "web"
+        Name = "Mossy"
+        Type = "web"
     }
     depends_on = [aws_security_group.web_sg]
 } 
