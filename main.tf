000f0bbafaea8c781c2e96dfedf1b97ee47984e1
MossyCastle <46568873+MossyCastle@users.noreply.github.com>
2021-04-14 16:23:41 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,8 @@
-/*
-    variable "address_space" {
-//  description = "Specifies the AWS instance type."
-    address_space     = "10.0.0.0/16"
+
+    variable "vpcid" {
+       vpcid = "v"
     }
-*/
+
 
 provider "aws" {
   //version = "~> 2.0"
@@ -11,12 +10,19 @@ provider "aws" {
   region = "us-west-2"
 }
 
+
 resource aws_vpc "hashicat" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
-
+ 
   // name = "mossy-terraform"
     tags = {
     Name = "terraform test"
+    mainvpc = vpc.id    
   }
+}
+
+resource "aws_subnet" Main-subnet"{
+   vpc_id  
+   subnet = "10.1.1.0/24"
 }
