6c132625afa5be6500aa642a9cac2936b16b37e5
MossyCastle <46568873+MossyCastle@users.noreply.github.com>
2021-04-14 16:13:30 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource aws_vpc "hashicat" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
 
-  name = "mossy-terraform"
+  // name = "mossy-terraform"
     tags = {
     Name = "terraform test"
   }
