b55cbb333377d5b484cb5073e75ee2eaaab9bd09
MossyCastle <46568873+MossyCastle@users.noreply.github.com>
2021-04-14 16:37:30 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,5 +21,8 @@ resource aws_vpc "hashicat" {
 
 resource aws_subnet "Main-subnet"{
    vpc_id = aws_vpc.hashicat.id    
-   cidr_block = "10.1.1.0/24"
+   cidr_block = "10.0.1.0/24"
+  tags {
+    name = terraform-subnet"
+}
 }
