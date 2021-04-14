229790a275463287e0effc0f20f000f23cdfe79b
MossyCastle <46568873+MossyCastle@users.noreply.github.com>
2021-04-14 16:00:54 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
-variable "address_space" {
+/*
+    variable "address_space" {
 //  description = "Specifies the AWS instance type."
     address_space     = "10.0.0.0/16"
     }
-
+*/
 
 provider "aws" {
   //version = "~> 2.0"
@@ -11,7 +12,7 @@ provider "aws" {
 }
 
 resource aws_vpc "hashicat" {
-  cidr_block           = var.address_space
+  cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
 
   tags = {
