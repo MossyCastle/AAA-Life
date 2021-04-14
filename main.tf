51f2d3206ed7cd78fb578304c6caae0db02f6311
MossyCastle <46568873+MossyCastle@users.noreply.github.com>
2021-04-14 15:56:42 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+variable "address_space" {
+//  description = "Specifies the AWS instance type."
+    address_space     = "10.0.0.0/16"
+
+
+
 provider "aws" {
   //version = "~> 2.0"
   //  region  = region
