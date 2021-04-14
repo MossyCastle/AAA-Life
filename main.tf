1dbf24a0dcfc51030b43582a19ec88a9e8519b34
MossyCastle <46568873+MossyCastle@users.noreply.github.com>
2021-04-14 15:50:47 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  version = "~> 2.0"
-  // region  = var.region
+  //version = "~> 2.0"
+  //  region  = region
   region = "us-west-2"
 }
 
 resource aws_vpc "hashicat" {
-  cidr_block           = var.address_space
+  cidr_block           = address_space
   enable_dns_hostnames = true
 
   tags = {
